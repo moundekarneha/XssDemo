ad1b7945672f5b6954933f1b13ae3e119e3d94fb
HP <HP@DESKTOP-G4J7E8F>
2022-08-26 16:19:34 +0530

    Add project files.

---
+++ XssDemo/Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XssDemo.Models
+{
+    public class Emp
+    {
+        [AllowHtml]
+        public string Name { get; set; }
+        public string LName { get; set; }
+        public string FName { get; set; }
+    }
+}
